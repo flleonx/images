a5226f892191661276cdb4ca6e339b11b959ad7b
flleonx <flleondev@gmail.com>
2025-04-11 13:29:48 +0200

    Update workflow

--- .github/scripts/format.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-files=$(for file in "$@"; do echo "$file" | sed 's/\/Dockerfile$//'; done | awk '!a[$0]++' | jq -ncR '[inputs | select(length>0)]')
-echo "files=${files}" >> $GITHUB_OUTPUT
