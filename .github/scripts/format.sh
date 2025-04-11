40626097397134a69e2def70d3744164d1800d37
flleonx <flleondev@gmail.com>
2025-04-11 11:56:31 +0200

    Change

--- .github/scripts/format.sh
+++ .github/scripts/format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 files=$(for file in "$@"; do echo "$file" | sed 's/\/Dockerfile$//'; done | awk '!a[$0]++' | jq -ncR '[inputs | select(length>0)]')
-echo "files=${files}"
+echo "files=${files}" >> $GITHUB_OUTPUT
