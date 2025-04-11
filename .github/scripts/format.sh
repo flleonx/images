4efe3fec60cf1b066a4c936a8373b2d0e1dc1294
flleonx <flleondev@gmail.com>
2025-04-11 11:18:18 +0200

    first commit

---
+++ .github/scripts/format.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+files=$(for file in "$@"; do echo "$file" | sed 's/\/Dockerfile$//'; done | awk '!a[$0]++' | jq -nR '[inputs | select(length>0)]')
+echo "files=${files}" >> $GITHUB_OUTPUT
